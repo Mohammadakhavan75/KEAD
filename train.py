9f0212d531c05e2e0d6c3fde1251663d7c646fc1
mohammadakhavan75 <mohammad.akhavan75@gmail.com>
2024-03-24 11:29:28 +0000

    chaning loss of train function into contrastive

--- train.py
+++ train.py
@@ -60,39 +60,79 @@ def parsing():
     return args
 
 
-def train(loader, net, train_global_iter, criterion, optimizer, device, writer):
+def train(train_loader, positives, negetives, net, train_global_iter, criterion, optimizer, device, writer):
 
     print("Traning...")
     net = net.to(device)
     net.train()  # enter train mode
 
     # track train classification accuracy
-    epoch_accuracies = []
-    epoch_loss = []
-    for inputs, targets in tqdm(loader):
+    epoch_accuracies = {
+        'normal': [],
+        'positive': [],
+        'negative': []
+    }
+    epoch_loss = {
+        'loss': [],
+        'ce': [],
+        'contrastive': []
+    }
+    for normal, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9 in zip(train_loader,
+                       positives[0], positives[1], positives[2], positives[3], positives[4],
+                       positives[5], positives[6], positives[7], positives[8], positives[9],
+                       negetives[0], negetives[1], negetives[2], negetives[3], negetives[4],
+                       negetives[5], negetives[6], negetives[7], negetives[8], negetives[9]):
+        ps = [p0[0], p1[0], p2[0], p3[0], p4[0], p5[0], p6[0], p7[0], p8[0], p9[0]]
+        ns = [n0[0], n1[0], n2[0], n3[0], n4[0], n5[0], n6[0], n7[0], n8[0], n9[0]]
         
-        inputs , targets = inputs.to(device) , targets.to(device)
+        imgs, labels = normal
+        imgs, labels = imgs.to(args.device), labels.to(args.device)
+        positive_imgs = torch.zeros_like(imgs)
+        negative_imgs = torch.zeros_like(imgs)
+        for i, label in enumerate(labels):
+            positive_imgs[i] = ps[label][i].to(args.device)
+            negative_imgs[i] = ns[label][i].to(args.device)
+            # positive_imgs, negative_imgs = np.asarray(positive_imgs), np.asarray(negative_imgs)
 
         optimizer.zero_grad()
-        preds, features_list = net(inputs, True)
-        probs = torch.softmax(preds, dim=1)
+        preds, normal_features = model(imgs, True)
+        positive_preds, positive_features = model(positive_imgs, True)
+        negative_preds, negative_features = model(negative_imgs, True)
+
+        normal_probs = torch.softmax(preds, dim=1)
+        positive_probs = torch.softmax(positive_preds, dim=1)
+        negative_probs = torch.softmax(negative_preds, dim=1)
 
         # Calculate loss contrastive for layer
-        loss_low_level = torch.zeros(features_list[-1].shape[1])
-        for i in range(features_list[-1].shape[1]):
-            single_filter = features_list[-1][:,i,:,:]
-        loss_contrast = contrastive()
-        loss_ce = criterion(preds, targets)
-        loss = loss_ce + loss_contrast
+        loss_contrastive = 0
+        for norm_f, pos_f, neg_f in zip(normal_features[-1], positive_features[-1], negative_features[-1]):
+            loss_contrastive = loss_contrastive + torch.sum(contrastive(norm_f, pos_f, neg_f))
 
-        output_index = torch.argmax(probs, dim=1)
-        acc = accuracy_score(list(to_np(output_index)), list(to_np(targets)))
-        epoch_accuracies.append(acc)
-        epoch_loss.append(loss.item())
+        loss_ce = criterion(preds, labels)
+        loss = loss_ce + loss_contrastive
+
+        normal_output_index = torch.argmax(normal_probs, dim=1)
+        positive_output_index = torch.argmax(positive_probs, dim=1)
+        negative_output_index = torch.argmax(negative_probs, dim=1)
+        acc_normal = accuracy_score(list(to_np(normal_output_index)), list(to_np(labels)))
+        acc_positive = accuracy_score(list(to_np(positive_output_index)), list(to_np(labels)))
+        acc_negative = accuracy_score(list(to_np(negative_output_index)), list(to_np(labels)))
+
+        # Logging section
+        epoch_accuracies['normal'].append(acc_normal)
+        epoch_accuracies['positive'].append(acc_positive)
+        epoch_accuracies['negative'].append(acc_negative)
+        epoch_loss['loss'].append(loss)
+        epoch_loss['ce'].append(loss_ce)
+        epoch_loss['contrastive'].append(loss_contrastive)
 
         train_global_iter += 1
-        writer.add_scalar("Train/loss", loss.item(), train_global_iter)
-        writer.add_scalar("Train/acc", acc, train_global_iter)
+        writer.add_scalar("Train/loss", loss, train_global_iter)
+        writer.add_scalar("Train/loss_ce", loss_ce.item(), train_global_iter)
+        writer.add_scalar("Train/loss_contrastive", loss_contrastive, train_global_iter)
+        writer.add_scalar("Train/acc_normal", acc_normal, train_global_iter)
+        writer.add_scalar("Train/acc_positive", acc_positive, train_global_iter)
+        writer.add_scalar("Train/acc_negative", acc_negative, train_global_iter)
 
         loss.backward()
         optimizer.step()
