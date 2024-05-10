be1f384e9c9a030fd0262e69a7f419ecca4b12ef
mohammadakhavan75 <mohammad.akhavan75@gmail.com>
2024-05-10 16:34:45 +0000

    adding simclr augmentation to training process

--- dataset_loader.py
+++ dataset_loader.py
@@ -11,6 +11,49 @@ from torch.utils.data.dataset import Subset
 from torch.utils.data import Dataset
 import transform_layers as TL
 from PIL import Image
+
+
+class GaussianBlur(object):
+    """blur a single image on CPU"""
+    def __init__(self, kernel_size):
+        radias = kernel_size // 2
+        kernel_size = radias * 2 + 1
+        self.blur_h = nn.Conv2d(3, 3, kernel_size=(kernel_size, 1),
+                                stride=1, padding=0, bias=False, groups=3)
+        self.blur_v = nn.Conv2d(3, 3, kernel_size=(1, kernel_size),
+                                stride=1, padding=0, bias=False, groups=3)
+        self.k = kernel_size
+        self.r = radias
+
+        self.blur = nn.Sequential(
+            nn.ReflectionPad2d(radias),
+            self.blur_h,
+            self.blur_v
+        )
+
+        self.pil_to_tensor = transforms.ToTensor()
+        self.tensor_to_pil = transforms.ToPILImage()
+
+    def __call__(self, img):
+        img = self.pil_to_tensor(img).unsqueeze(0)
+
+        sigma = np.random.uniform(0.1, 2.0)
+        x = np.arange(-self.r, self.r + 1)
+        x = np.exp(-np.power(x, 2) / (2 * sigma * sigma))
+        x = x / x.sum()
+        x = torch.from_numpy(x).view(1, -1).repeat(3, 1)
+
+        self.blur_h.weight.data.copy_(x.view(3, 1, self.k, 1))
+        self.blur_v.weight.data.copy_(x.view(3, 1, 1, self.k))
+
+        with torch.no_grad():
+            img = self.blur(img)
+            img = img.squeeze()
+
+        img = self.tensor_to_pil(img)
+
+        return img
+    
 class SVHN(Dataset):
     url = ""
     filename = ""
@@ -333,18 +376,29 @@ def noise_loader(batch_size=32, num_workers=32, one_class_idx=None, tail_positiv
 
 def load_cifar10(cifar10_path, batch_size=32, num_workers=32, one_class_idx=None, tail_normal=None):
 
-    mean = [x / 255 for x in [125.3, 123.0, 113.9]]
-    std = [x / 255 for x in [63.0, 62.1, 66.7]]
+    # mean = [x / 255 for x in [125.3, 123.0, 113.9]]
+    # std = [x / 255 for x in [63.0, 62.1, 66.7]]
 
     # train_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean, std)])
     # test_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean, std)])
-    train_transform = transforms.Compose([transforms.ToTensor()])
-    test_transform = transforms.Compose([transforms.ToTensor()])
+    # train_transform = transforms.Compose([transforms.ToTensor()])
+    # test_transform = transforms.Compose([transforms.ToTensor()])
+
+    s = 1
+    size = 32
+    color_jitter = transforms.ColorJitter(0.8 * s, 0.8 * s, 0.8 * s, 0.2 * s)
+    data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=size),
+                                            transforms.RandomHorizontalFlip(),
+                                            transforms.RandomApply([color_jitter], p=0.8),
+                                            transforms.RandomGrayscale(p=0.2),
+                                            GaussianBlur(kernel_size=int(0.1 * size)),
+                                            transforms.ToTensor()])
+
 
     train_data = torchvision.datasets.CIFAR10(
-        cifar10_path, train=True, transform=train_transform, download=True)
+        cifar10_path, train=True, transform=data_transforms, download=True)
     test_data = torchvision.datasets.CIFAR10(
-        cifar10_path, train=False, transform=test_transform, download=True)
+        cifar10_path, train=False, transform=data_transforms, download=True)
 
     if one_class_idx != None:
         train_data = get_subclass_dataset(train_data, one_class_idx)
