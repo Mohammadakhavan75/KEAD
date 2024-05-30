fd8c856b996c4584ef940d75f278ccea17db57d3
Mohammad <mohammad.akhavan75@gmail.com>
2024-05-30 18:56:24 +0330

    creating representations for KE

--- create_representations.py
+++ create_representations.py
@@ -30,12 +30,14 @@ def parsing():
                         help='seed')
     parser.add_argument('--num_workers', type=int, 
                         default=0, help='number of workers')
+    parser.add_argument('--backbone', type=str, 
+                        default='clip', help='clip or dinov2')
     parser.add_argument('--dataset', type=str, 
                         default='cifar10', help='cifar10-cifar100-svhn')
     parser.add_argument('--save_rep_norm', action='store_true',
-                        help='cifar10-cifar100-svhn')
+                        help='saving representation for normal data')
     parser.add_argument('--save_rep_aug', action='store_true',
-                        help='cifar10-cifar100-svhn')
+                        help='saving representation for normal data')
     parser.add_argument('--gpu', default='0', type=str, help='gpu number')
     args = parser.parse_args()
     return args
@@ -49,8 +51,13 @@ torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
 device = f'cuda:{args.gpu}'
-model, transform = clip.load("ViT-L/14", device=device)
 
+if args.backbone == 'clip':
+    model, transform = clip.load("ViT-L/14", device=device)
+elif args.backbone == 'dinov2':
+    model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitg14_reg')
+    transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),
+                                        torchvision.transforms.Resize(224, 224)])
 with open('config.json') as config_file:
     config = json.load(config_file)
 
@@ -61,9 +68,9 @@ generalization_path = config['generalization_path']
 
 
 if args.save_rep_norm:
-    os.makedirs(f'./representations/{args.dataset}/', exist_ok=True)
+    os.makedirs(f'./representations/{args.backbone}/{args.dataset}/', exist_ok=True)
 if args.save_rep_aug:
-    os.makedirs(f'./representations/{args.dataset}/{args.aug}/', exist_ok=True)
+    os.makedirs(f'./representations/{args.backbone}/{args.dataset}/{args.aug}/', exist_ok=True)
 
 if args.dataset == 'cifar10':
     train_aug_imgs_path = os.path.join(generalization_path, f'CIFAR10_Train_AC/{args.aug}.npy')
@@ -91,10 +98,11 @@ elif args.dataset == 'imagenet30':
     train_aug_imgs_path = os.path.join(generalization_path, f'Imagenet_Train_AC/{args.aug}.npy')
     train_aug_targets_path = os.path.join(generalization_path, 'Imagenet_Train_AC/labels_train.npy')
     
-    train_transforms = torchvision.transforms.Compose([
-    torchvision.transforms.Resize(256),
-    torchvision.transforms.CenterCrop(224),
-    torchvision.transforms.ToTensor()])
+    if args.backbone != 'clip':
+        transform = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(256),
+        torchvision.transforms.CenterCrop(224),
+        torchvision.transforms.ToTensor()])
 
     noraml_dataset = torchvision.datasets.ImageNet(root=imagenet_path, split='train', transform=transform)
     aug_dataset = load_np_dataset(train_aug_imgs_path, train_aug_targets_path, transform=transform, dataset='imagenet30')
@@ -124,10 +132,10 @@ for i, data in enumerate(loader):
         imgs_aug_features = model.encode_image(imgs_aug)
         # Saving the representations
         if args.save_rep_norm:
-            with open(f'./representations/{args.dataset}/batch_{i}.pkl', 'wb') as f:
+            with open(f'./representations/{args.backbone}/{args.dataset}/batch_{i}.pkl', 'wb') as f:
                 pickle.dump(imgs_n_features, f)
         if args.save_rep_aug:
-            with open(f'./representations/{args.dataset}/{args.aug}/batch_{i}.pkl', 'wb') as f:
+            with open(f'./representations/{args.backbone}/{args.dataset}/{args.aug}/batch_{i}.pkl', 'wb') as f:
                 pickle.dump(imgs_aug_features, f)
 
         for f_n, f_a in zip(imgs_n_features, imgs_aug_features):
@@ -144,10 +152,10 @@ for i, data in enumerate(loader):
 
     # Saving the representations
     if args.save_rep_norm:
-        with open(f'./representations/{args.dataset}/batch_{i}.pkl', 'wb') as f:
+        with open(f'./representations/{args.backbone}/{args.dataset}/batch_{i}.pkl', 'wb') as f:
             pickle.dump(imgs_n_features, f)
     if args.save_rep_aug:
-        with open(f'./representations/{args.dataset}/{args.aug}/batch_{i}.pkl', 'wb') as f:
+        with open(f'./representations/{args.backbone}/{args.dataset}/{args.aug}/batch_{i}.pkl', 'wb') as f:
             pickle.dump(imgs_aug_features, f)
 
     for f_n, f_a in zip(imgs_n_features, imgs_aug_features):
@@ -162,18 +170,18 @@ targets_list = np.asarray(targets_list)
 
 
 
-os.makedirs(f'./saved_pickles/{args.dataset}/', exist_ok=True)
-os.makedirs(f'./saved_pickles/{args.dataset}/diffs/', exist_ok=True)
-os.makedirs(f'./saved_pickles/{args.dataset}/targets/', exist_ok=True)
-os.makedirs(f'./saved_pickles/{args.dataset}/cosine/', exist_ok=True)
-os.makedirs(f'./saved_pickles/{args.dataset}/wasser/', exist_ok=True)
+os.makedirs(f'./saved_pickles/{args.backbone}/{args.dataset}/', exist_ok=True)
+os.makedirs(f'./saved_pickles/{args.backbone}/{args.dataset}/diffs/', exist_ok=True)
+os.makedirs(f'./saved_pickles/{args.backbone}/{args.dataset}/targets/', exist_ok=True)
+os.makedirs(f'./saved_pickles/{args.backbone}/{args.dataset}/cosine/', exist_ok=True)
+os.makedirs(f'./saved_pickles/{args.backbone}/{args.dataset}/wasser/', exist_ok=True)
 
 
-with open(f'./saved_pickles/{args.dataset}/targets/{args.aug}.pkl', 'wb') as f:
+with open(f'./saved_pickles/{args.backbone}/{args.dataset}/targets/{args.aug}.pkl', 'wb') as f:
     pickle.dump(targets_list, f)
-with open(f'./saved_pickles/{args.dataset}/diffs/{args.aug}.pkl', 'wb') as f:
+with open(f'./saved_pickles/{args.backbone}/{args.dataset}/diffs/{args.aug}.pkl', 'wb') as f:
     pickle.dump(euclidean_diffs, f)
-with open(f'./saved_pickles/{args.dataset}/cosine/{args.aug}.pkl', 'wb') as f:
+with open(f'./saved_pickles/{args.backbone}/{args.dataset}/cosine/{args.aug}.pkl', 'wb') as f:
     pickle.dump(cosine_diff, f)
-with open(f'./saved_pickles/{args.dataset}/wasser/{args.aug}.pkl', 'wb') as f:
+with open(f'./saved_pickles/{args.backbone}/{args.dataset}/wasser/{args.aug}.pkl', 'wb') as f:
     pickle.dump(wasser_diff, f)
