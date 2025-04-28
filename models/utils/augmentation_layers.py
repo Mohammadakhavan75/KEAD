775c3efad1688bf061fdec3f4a1bdf9561a6bc09
Mohammad <mohammad.akhavan75@gmail.com>
2025-04-28 18:21:45 +0330

    resolve numpy deprication error np.float_

--- models/utils/augmentation_layers.py
+++ models/utils/augmentation_layers.py
@@ -193,7 +193,7 @@ def plasma_fractal(mapsize=32, wibbledecay=3):
         raise ValueError(f"mapsize must be a power of 2, got {mapsize}")
 
     try:
-        maparray = np.empty((mapsize, mapsize), dtype=np.float16)
+        maparray = np.empty((mapsize, mapsize), dtype=np.float32)
         maparray[0, 0] = 0
         stepsize = mapsize
         wibble = 100
