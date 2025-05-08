e4c1862520c9a09349a130020c64e784a8f0558e
Mohammad <mohammad.akhavan75@gmail.com>
2025-05-08 17:29:07 +0330

    debug

--- utils/paths.py
+++ utils/paths.py
@@ -12,7 +12,7 @@ def create_path(args):
 
     save_path = root_addr + f'{args.dataset}' + f'_one_class_idx_{args.one_class_idx}' + \
                 f'_{args.model}' + f'_{args.img_size}' + f'_{args.optimizer}' + f'_lr_{args.learning_rate}' + \
-                f'_epochs_{args.epochs}' + f'_tau_{args.tau}' + f'_seed_{args.seed}' + f'_k_view_{args.k_view}' + '/'
+                f'_epochs_{args.epochs}' + f'_tau_{args.temperature}' + f'_seed_{args.seed}' + f'_k_view_{args.k_view}' + '/'
     
     model_save_path = save_path + 'models/'
     os.makedirs(model_save_path, exist_ok=True)
