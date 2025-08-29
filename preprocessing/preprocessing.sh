6ead15697f903ffdef098467be86a712b1acdc7c
Mohammadakhavan75 <mohammad.akhavan75@gmail.com>
2025-08-29 23:49:27 +0330

    new args

--- preprocessing/preprocessing.sh
+++ preprocessing/preprocessing.sh
@@ -4,42 +4,42 @@ dataset=$1
 config=$2
 backbone=$3
 batch_size=$4
-
+seed=$5 
 # creating train augmentations datatset
-cat ./preprocessing/selected_noises.txt | while read -r noise
-do
-    echo "running on $noise"
-    python  ./preprocessing/create_augmentation.py --dataset $dataset --config $config --severity 1 --train --aug $noise &
-done
+# cat ./preprocessing/selected_noises.txt | while read -r noise
+# do
+#     echo "running on $noise"
+#     python  ./preprocessing/create_augmentation.py --dataset $dataset --config $config --severity 1 --train --aug $noise --seed $seed &
+# done
 
-# creating test augmentations datatset
-cat ./preprocessing/selected_noises.txt | while read -r noise
-do
-    echo "running on $noise"
-    python ./preprocessing/create_augmentation.py --dataset $dataset --config $config --severity 5 --aug $noise &
-done
+# # creating test augmentations datatset
+# cat ./preprocessing/selected_noises.txt | while read -r noise
+# do
+#     echo "running on $noise"
+#     python ./preprocessing/create_augmentation.py --dataset $dataset --config $config --severity 5 --aug $noise --seed $seed &
+# done
 
 # creating representation from selected backbone and dataset
-C=0
-cat ./preprocessing/selected_noises.txt | while read -r noise
-do
-    echo "running on $noise"
-    if [ $C -eq 0 ]
-    then
-        python ./preprocessing/create_representations.py --aug $noise --dataset $dataset --config $config --backbone $backbone --batch_size $batch_size --save_rep_norm --save_rep_aug --gpu 0
-        C=1
-    else
-        python ./preprocessing/create_representations.py --aug $noise --dataset $dataset --config $config --backbone $backbone --batch_size $batch_size --save_rep_aug --gpu 0 
-    fi
-done
+# C=0
+# cat ./preprocessing/selected_noises.txt | while read -r noise
+# do
+#     echo "running on $noise"
+#     if [ $C -eq 0 ]
+#     then
+#         python ./preprocessing/create_representations.py --aug $noise --dataset $dataset --config $config --backbone $backbone --batch_size $batch_size --seed $seed --save_rep_norm --save_rep_aug --gpu 0
+#         C=1
+#     else
+#         python ./preprocessing/create_representations.py --aug $noise --dataset $dataset --config $config --backbone $backbone --batch_size $batch_size --seed $seed --save_rep_aug --gpu 0
+#     fi
+# done
 
 # creating wasserstein distances on dataset and augmented dataset
 cat ./preprocessing/selected_noises.txt | while read -r noise
 do
     echo "running on $noise"
-    python ./preprocessing/wasser_dist.py --aug $noise --dataset $dataset --backbone $backbone --config $config --one_class &
+    python ./preprocessing/wasser_dist.py --aug $noise --dataset $dataset --backbone $backbone --config $config --seed $seed --one_class
 done
 
 # sleep 600
 # # combine distances for dataset into a dictionary
-python ./preprocessing/dist_combination.py --dataset $dataset --backbone $backbone --config $config --one_class
+# python ./preprocessing/dist_combination.py --dataset $dataset --backbone $backbone --config $config --one_class
